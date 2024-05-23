fecc5b5176eb7e301fa90693bfbbeedf625e7399
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2024-05-23 10:19:25 +0100

    codon table and lookup

--- app/controllers/codons_controller.rb
+++ app/controllers/codons_controller.rb
@@ -4,6 +4,8 @@ class CodonsController < ApplicationController
   end
 
   def convert
+    @codons = params[:sequence].upcase.gsub(/[^ATGC]/, '').scan(/.{3}/).map { |c| Codons::MAP[c] }
+
     if turbo_frame_request?
       render partial: 'convert'
     else
