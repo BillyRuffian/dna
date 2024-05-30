7c3f69fd7e3b0c037445bff49322c64013bc182c
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2024-05-30 21:15:45 +0100

    better codon-amino output

--- app/controllers/codons_controller.rb
+++ app/controllers/codons_controller.rb
@@ -4,7 +4,8 @@ class CodonsController < ApplicationController
   end
 
   def convert
-    @codons = params[:sequence].upcase.gsub(/[^ATGC]/, '').scan(/.{3}/).map { |c| Codons::MAP[c] }
+    @codons = params[:sequence].upcase.gsub(/[^ATGC]/, '').scan(/.{3}/)
+    @aminos = @codons.map { |c| Codons::MAP[c] }
 
     if turbo_frame_request?
       render partial: 'convert'
