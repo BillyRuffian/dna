e5fcde607f4d24a2a7338c6bfa75acaf6e1c4383
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2024-06-04 21:01:42 +0100

    clean up

--- app/controllers/codons_controller.rb
+++ app/controllers/codons_controller.rb
@@ -6,18 +6,7 @@ class CodonsController < ApplicationController
   def convert
     @sequence = Bio::Sequence::NA.new(params[:sequence])
     @codon_table = params[:codon_table]&.to_i || 1
-
-    if turbo_frame_request?
-      render partial: 'convert'
-    else
-      warn 'What the hell?'
-    end
-  end
-
-  def convert_x
-    @codons = params[:sequence].upcase.gsub(/[^ATGC]/, '').scan(/.{3}/)
-    @aminos = @codons.map { |c| Codons::MAP[c] }
-    @stop_codon = Codons::ACIDS[:stp]
+    @aminos = @sequence.translate(1, @codon_table)
 
     if turbo_frame_request?
       render partial: 'convert'
